762487e1ef0a00110822f6ae0190f638e2782ed4
Muhamad Azmy <muhamada@greenitglobe.com>
2018-02-19 11:07:39 +0200

    Use main from cloud-controller-manager

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	goflag "flag"
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 
 	"k8s.io/apiserver/pkg/server/healthz"
-	"k8s.io/apiserver/pkg/util/flag"
+	utilflag "k8s.io/apiserver/pkg/util/flag"
 	"k8s.io/apiserver/pkg/util/logs"
 	"k8s.io/kubernetes/cmd/cloud-controller-manager/app"
-	"k8s.io/kubernetes/cmd/cloud-controller-manager/app/options"
 	_ "k8s.io/kubernetes/pkg/client/metrics/prometheus" // for client metric registration
 	_ "k8s.io/kubernetes/pkg/version/prometheus"        // for version metric registration
-	"k8s.io/kubernetes/pkg/version/verflag"
 
 	_ "github.com/openvcloud/ovc-controller-manager/ovc"
 
@@ -23,23 +24,21 @@ func init() {
 }
 
 func main() {
-	s := options.NewCloudControllerManagerOptions()
-	s.AddFlags(pflag.CommandLine)
+	rand.Seed(time.Now().UTC().UnixNano())
 
-	flag.InitFlags()
+	command := app.NewCloudControllerManagerCommand()
+
+	// TODO: once we switch everything over to Cobra commands, we can go back to calling
+	// utilflag.InitFlags() (by removing its pflag.Parse() call). For now, we have to set the
+	// normalize func and add the go flag set by hand.
+	pflag.CommandLine.SetNormalizeFunc(utilflag.WordSepNormalizeFunc)
+	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
+	// utilflag.InitFlags()
 	logs.InitLogs()
 	defer logs.FlushLogs()
 
-	verflag.PrintAndExitIfRequested()
-
-	cfg, err := s.Config()
-	if err != nil {
-		//todo: proper error handling here
-		panic(err)
-	}
-
-	if err := app.Run(cfg.Complete()); err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+	if err := command.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 }
